836566073ae73fe6dea88d78d4737d483cbf4064
Per Kristian Coskun <47058816+Zartok89@users.noreply.github.com>
2024-09-29 17:52:08 +0200

    Initiate Project

---
+++ Source/Oblig2/Oblig2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Oblig2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Oblig2, "Oblig2" );
